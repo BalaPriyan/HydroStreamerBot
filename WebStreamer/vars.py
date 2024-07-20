0aa6007a5f33e8b8b2925ed185b700ea8be67240
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-07-21 00:31:38 +0530

    Add files via upload

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -12,23 +12,27 @@ class Var(object):
     API_HASH = str(environ.get("API_HASH"))
     BOT_TOKEN = str(environ.get("BOT_TOKEN"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
-    WORKERS = int(environ.get("WORKERS", "4"))  # 6 workers = 6 commands at once
-    BIN_CHANNEL = int(environ.get("BIN_CHANNEL"))  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
+    BIN_CHANNEL = int(
+        environ.get("BIN_CHANNEL")
+    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(environ.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
     FQDN = str(environ.get("FQDN", BIND_ADDRESS))
-    URL = "http{}://{}{}/".format("s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT))
-    
-    DATABASE_URL = str(environ.get('DATABASE_URL',""))
-    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "Teleggram"))
+    URL = "http{}://{}{}/".format(
+            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
+        )
+
+    DATABASE_URL = str(environ.get('DATABASE_URL'))
+    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "Telegram"))
     OWNER_ID = int(environ.get('OWNER_ID'))
     SESSION_NAME = str(environ.get('SESSION_NAME', 'hydroBot'))
     FORCE_UPDATES_CHANNEL = environ.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_UPDATES_CHANNEL = True if str(FORCE_UPDATES_CHANNEL).lower() == "true" else False
-    ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS") or "").split(",") if x.strip("@ ")]
+    ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS", "7136104373") or "").split(",") if x.strip("@ ")]
 
     KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "0").lower()) in  ("1", "true", "t", "yes", "y")
     IMAGE_FILEID = environ.get('IMAGE_FILEID', "https://deekshith.eu.org/static/MyFiles.png")
@@ -41,8 +45,3 @@ class Var(object):
     MODE = environ.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     LINK_LIMIT = int(environ.get("LINK_LIMIT")) if "LINK_LIMIT" in environ else None
-    
-    #SHOTENER FUNCTION
-    SHORTENER = environ.get('SHORTENER', False)
-    SHORTLINK_API = environ.get('SHORTLINK_API')
-    SHORTLINK_UR = environ.get('SHORTLINK_UR')
