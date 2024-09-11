0566718e66a655912a8e9535cad43f6410926f94
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-09-11 07:03:50 +0530

    Port Fix

--- HydroStreamer/vars.py
+++ HydroStreamer/vars.py
@@ -13,10 +13,8 @@ class Var(object):
     BOT_TOKEN = str(environ.get("BOT_TOKEN"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL")
-    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
-    PORT = int(environ.get("PORT", 8080))
+    BIN_CHANNEL = int(environ.get("BIN_CHANNEL"))  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    PORT = int(environ.get("PORT", "8080"))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
